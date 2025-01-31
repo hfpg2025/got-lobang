b281b51687352e0a977215f16eb3ab8ef2f70fa1
LoneRifle <LoneRifle@users.noreply.github.com>
2025-01-31 14:50:46 +0800

    fix: add tos, privacy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
       <body>
         <HackathonBanner />
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <div className="px-4 flex flex-row gap-2">
+          <a href="https://docs.google.com/document/d/1xvOPQwm2eYIfG8fftJ3yRPvowR7n4CUP/edit?usp=sharing&ouid=115377118495282373162&rtpof=true&sd=true" target="_blank">Terms of Use</a>
+          <a href="https://docs.google.com/document/d/1-0x1wpK5kGsH6qD_x4PX6gwL5Aa6qfay/edit" target="_blank">Privacy Policy</a>
+        </div>
       </body>
     </html>
   );
