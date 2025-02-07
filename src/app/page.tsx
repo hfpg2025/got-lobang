afc020e26cc3621328cb5483633da922ecc39f8a
LoneRifle <LoneRifle@users.noreply.github.com>
2025-02-07 09:53:45 +0800

    fix: add link back to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default async function Home({
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#15162c] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Got <span className="text-[hsl(280,100%,70%)]">Lobang</span>?
+            <Link href="/">Got <span className="text-[hsl(280,100%,70%)]">Lobang</span>?</Link>
           </h1>
           <h1 className="text-3xl tracking-tight p-6">
             {metadata.description}
